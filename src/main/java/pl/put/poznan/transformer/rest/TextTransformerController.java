4cfb72b041ec8a01174a9eb721f777c7050aa1d2
Michał Szczepaniak <49671638+Szczepaniak-M@users.noreply.github.com>
2020-12-01 18:45:14 +0100

    Notification Test

--- src/main/java/pl/put/poznan/transformer/rest/TextTransformerController.java
+++ src/main/java/pl/put/poznan/transformer/rest/TextTransformerController.java
@@ -4,7 +4,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import pl.put.poznan.transformer.logic.TextTransformer;
 
-
+import java.util.Arrays;
 
 
 @RestController
