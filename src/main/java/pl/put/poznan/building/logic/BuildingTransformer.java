e0ff35acdf35256398fac34053f104ba8109abf0
Szczepaniak-M <m.szczepaniak99@gmail.com>
2020-12-11 13:09:50 +0100

    Rename classes and package

--- src/main/java/pl/put/poznan/building/logic/BuildingTransformer.java
+++ src/main/java/pl/put/poznan/building/logic/BuildingTransformer.java
@@ -1,13 +1,13 @@
-package pl.put.poznan.transformer.logic;
+package pl.put.poznan.building.logic;
 
 /**
  * This is just an example to show that the logic should be outside the REST service.
  */
-public class TextTransformer {
+public class BuildingTransformer {
 
     private final String[] transforms;
 
-    public TextTransformer(String[] transforms){
+    public BuildingTransformer(String[] transforms){
         this.transforms = transforms;
     }
 
